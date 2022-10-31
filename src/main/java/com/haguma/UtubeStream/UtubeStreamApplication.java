6fea8eb239f60d63a425321fdc7d013e5cc8ada9
fabrice137 <fabruz3@gmail.com>
2022-10-31 10:28:34 +0200

    Initialized my Project

---
+++ src/main/java/com/haguma/UtubeStream/UtubeStreamApplication.java
@@ -0,0 +1,13 @@
+package com.haguma.UtubeStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtubeStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtubeStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haguma/UtubeStream/UtubeStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haguma.UtubeStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtubeStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
